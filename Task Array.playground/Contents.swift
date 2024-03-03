ff006309398c2f7bf94ef53bde2e08657388edf7
Feras Alshadad <ferasalshadad@Ferass-Laptop.local>
2024-03-03 15:29:04 +0300

    Added files

---
+++ Task Array.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//Task: Array
+var books = ["Rook","Took","Nook", "Wok","Joke"]
+print("The count for the books array is",(books.count))
+
+books.append("Hoof")
+books.insert("Roof", at: 1)
+books.remove(at: 4)
